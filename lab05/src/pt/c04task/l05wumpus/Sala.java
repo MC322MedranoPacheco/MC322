11227b3e6978b112d7d213d516c05c7b6b936a59
gmedranos <102122081+gmedranos@users.noreply.github.com>
2022-05-11 19:03:32 -0300

    Adiciona a classe sla

---
+++ lab05/src/pt/c04task/l05wumpus/Sala.java
@@ -0,0 +1,69 @@
+package pt.c40task.l05wumpus;
+
+public class Sala {
+	public Componente[] componentes = new Componente[4];
+	public boolean descoberta = false;
+	public int numComponentes = 0;
+	
+	public void descobrir() {
+		descoberta = true;
+	}
+	
+	private String maiorPrioridade() {
+		String tipoPeca;
+		for (int i = 0; i < numComponentes; i++) {
+			tipoPeca = componentes[i].toString();
+			if (tipoPeca == "W" || tipoPeca == "B" || tipoPeca == "O")
+				return tipoPeca;
+		}
+		for (int i = 0; i < numComponentes; i++) {
+			tipoPeca = componentes[i].toString();
+			if (tipoPeca == "H")
+				return tipoPeca;
+		}
+		for (int i = 0; i < numComponentes; i++) {
+			tipoPeca = componentes[i].toString();
+			if (tipoPeca == "f")
+				return tipoPeca;
+		}
+		else return "b";
+			
+	}
+	
+	public String toString() {
+		String string;
+		if (!descoberta)
+			string = "-";
+		else if(numComponentes == 0)
+			string = "#";
+		else
+			string = componentes[0].toString();
+		return string;		
+	}
+	
+	// Adiciona um componente. O componente de maior prioridade fica sempre na primeira posicao
+	public void adicionarComponente(Componente componente) {
+		int i = 0;
+		while(componentes[i] != null)
+			i++;
+		componentes[i] = componente;
+		numComponentes++;
+	}
+	
+	public boolean procurarComponente(String componente) {
+		boolean bool = false;
+		for (int i = 0; i < numComponentes; i++) {
+			if(componentes[i].toString() == componente)
+				bool = true;
+			}
+		return bool;
+	}
+	
+	public void removerComponente(String componente) {
+		for (int i = 0; i < numComponentes; i++)
+			if (componentes[i].toString() == componente)
+				componentes[i] = null;
+		numComponentes--;	
+	}
+}
+	
