c15208aa0ba223bb50d1dacf02e6c700845834fa
HenriquePManoel <102121798+HenriquePManoel@users.noreply.github.com>
2022-05-11 18:56:08 -0300

    readd componente

---
+++ lab05/src/pt/c04task/l05wumpus/Componente.java
@@ -0,0 +1,22 @@
+package l05wumpus;
+
+public abstract class Componente {
+	public Posicao posicao;
+	public Caverna caverna;
+
+	public Componente(int x, int y) {
+		setPosicao(x,y);
+	}
+
+	public Posicao getPosicao() {
+		return posicao;
+	}
+
+	public void setPosicao(int x, int y) {
+		posicao.setPosicaoX(x);
+		posicao.setPosicaoY(y);
+	}
+
+	public abstract String toString();
+	
+}
