c41980d7339c6eaec0b92238a76f6dfa49cad992
gmedranos <102122081+gmedranos@users.noreply.github.com>
2022-05-12 14:32:10 -0300

    Atualiza Caverna

--- lab05/src/pt/c04task/l05wumpus/Caverna.java
+++ lab05/src/pt/c04task/l05wumpus/Caverna.java
@@ -5,9 +5,9 @@ public class Caverna {
 	public Sala[][] salas;
 
 	public void moverComponente(String componente, Posicao posicaoInicial, Posicao posicaoFinal) {
-		private Componente mudado;
-		mudado = salas[posicaoInicial.getPosicaoY()][posicaoInicial.getPosicaoX()].removerComponente(componente);
-		salas[posicaoFinal.getPosicaoY()][posicaoFinal.getPosicaoX()].adicionarComponente(mudado);
+		Componente mudado;
+		mudado = salas[posicaoInicial.getY()][posicaoInicial.getX()].removerComponente(componente);
+		salas[posicaoFinal.getY()][posicaoFinal.getX()].adicionarComponente(mudado);
 	}
 
 }
