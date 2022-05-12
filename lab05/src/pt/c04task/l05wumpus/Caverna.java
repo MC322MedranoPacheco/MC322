918ca6c50ef6b95b2c15b3d51c73f585a507b5c6
gmedranos <102122081+gmedranos@users.noreply.github.com>
2022-05-12 15:43:08 -0300

    Adiciona metodos na caverna

--- lab05/src/pt/c04task/l05wumpus/Caverna.java
+++ lab05/src/pt/c04task/l05wumpus/Caverna.java
@@ -1,9 +1,26 @@
 package pt.c40task.l05wumpus;
 
 public class Caverna {
+	
 
-	public Sala[][] salas;
-
+	public Sala[][] salas = new Sala[4][4];
+	
+	public Caverna() {
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < 4;j++) {
+				salas[i][j] = new Sala();
+			}
+		}
+	}
+	
+	public void printCaverna() {
+		for (int i = 0; i < 4; i++) {
+			for(int k = 0; k < 4; k++)
+				System.out.print(salas[i][k] + " ");
+			System.out.println("");
+		}
+	}
+	
 	public void moverComponente(String componente, Posicao posicaoInicial, Posicao posicaoFinal) {
 		Componente mudado;
 		mudado = salas[posicaoInicial.getY()][posicaoInicial.getX()].removerComponente(componente);
