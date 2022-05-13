4736bf8c4be8e19a71f40a960cef6517717b95e7
Gabriel Medrano Silva <g247113@dac.unicamp.br>
2022-05-13 19:54:56 -0300

    Adiciona a classe montadora e modifica as outras classes

---
+++ lab05/src/pt/c04task/l05wumpus/AppWumpus.java
@@ -0,0 +1,28 @@
+package pt.c40task.l05wumpus;
+
+public class AppWumpus {
+
+   public static void main(String[] args) {
+      AppWumpus.executaJogo(
+            (args.length > 0) ? args[0] : null,
+            (args.length > 1) ? args[1] : null,
+            (args.length > 2) ? args[2] : null);
+   }
+   
+   public static void executaJogo(String arquivoCaverna, String arquivoSaida,
+                                  String arquivoMovimentos) {
+      Toolkit tk = Toolkit.start(arquivoCaverna, arquivoSaida, arquivoMovimentos);
+      
+      Caverna caverna = new Caverna();
+      caverna = MontadorCaverna.montar(arquivoCaverna, arquivoSaida, arquivoMovimentos);
+      for (int i = 0; i < 4; i ++)
+    	  for (int j = 0; j < 4; j++)
+    		  caverna.salas[i][j].descobrir();
+      caverna.printCaverna();
+      
+      caverna.printCaverna();
+      
+      tk.stop();
+   }
+
+}
---
+++ lab05/src/pt/c04task/l05wumpus/MontadorCaverna.java
@@ -0,0 +1,40 @@
+package pt.c40task.l05wumpus;
+
+public class MontadorCaverna {
+	public static Caverna montar(String arquivoCaverna, String arquivoSaida,
+            String arquivoMovimentos) {
+		Caverna caverna = new Caverna();
+		Toolkit tk = Toolkit.start(arquivoCaverna, arquivoSaida, arquivoMovimentos);
+		String caverna_str[][] = tk.retrieveCave();
+		
+		int numB = 0;
+		int numW = 0;
+		int numH = 0;
+		int numO = 0;
+		
+		for (int i = 0; i < caverna_str.length; i++) {
+				 Posicao pos = new Posicao(Integer.parseInt(caverna_str[i][1]) - 1, Integer.parseInt(caverna_str[i][0]) - 1);
+	        	 if(caverna_str[i][2].equals("P")) {
+	        		 numH++;
+	        		 Componente heroi = new Heroi(Integer.parseInt(caverna_str[i][1]), Integer.parseInt(caverna_str[i][0]));
+	        		 caverna.adicionarComponente(pos, heroi);
+	        	 }
+	        	 else if(caverna_str[i][2].equals("W")) {
+		        	 	numW++;
+		        	 	Componente wumpus = new Wumpus(Integer.parseInt(caverna_str[i][1]), Integer.parseInt(caverna_str[i][0]));
+		        	 	caverna.adicionarComponente(pos, wumpus);
+	        	 }
+	        	 else if(caverna_str[i][2].equals("B")) {
+		        	 	numB++;
+		        	 	Componente buraco = new Buraco(Integer.parseInt(caverna_str[i][1]), Integer.parseInt(caverna_str[i][0]));
+		        	 	caverna.adicionarComponente(pos, buraco);
+		         }
+	        	 else if(caverna_str[i][2].equals("O")) {
+		        	 	numO++;
+		        	 	Componente ouro = new Ouro(Integer.parseInt(caverna_str[i][1]), Integer.parseInt(caverna_str[i][0]));
+		        	 	caverna.adicionarComponente(pos, ouro);
+	        	 }
+		}
+	return caverna;
+	}
+}
