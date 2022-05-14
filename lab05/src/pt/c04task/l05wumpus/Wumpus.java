370908e3f8c60aa6bd4974347bec72994ce3d363
HenriquePManoel <102121798+HenriquePManoel@users.noreply.github.com>
2022-05-14 20:44:25 -0300

    Wumpus now construct fedor

--- lab05/src/pt/c04task/l05wumpus/Wumpus.java
+++ lab05/src/pt/c04task/l05wumpus/Wumpus.java
@@ -4,6 +4,13 @@ public class Wumpus extends Componente{
 
 	public Wumpus(int x, int y) {
 		super(x, y);
+		Posicao adjacentes[] = posicao.adjacentes_validos();
+		for(int i = 0; i < 4; i++) {
+			if(adjacentes[i] != null) {
+				Componente fedor = new Fedor(adjacentes[i].getX(), adjacentes[i].getY());
+				caverna.adicionarComponente(adjacentes[i], fedor);
+			}
+		}
 	}
 
 	public String toString() {
