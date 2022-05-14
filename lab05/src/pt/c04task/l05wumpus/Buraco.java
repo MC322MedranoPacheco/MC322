30d706cef01c0a89e19f5f21863151d08cc4e69a
HenriquePManoel <102121798+HenriquePManoel@users.noreply.github.com>
2022-05-14 20:43:49 -0300

    Buraco now construct brisas

--- lab05/src/pt/c04task/l05wumpus/Buraco.java
+++ lab05/src/pt/c04task/l05wumpus/Buraco.java
@@ -4,10 +4,18 @@ public class Buraco extends Componente{
 
 	public Buraco(int x, int y) {
 		super(x, y);
+		Posicao adjacentes[] = posicao.adjacentes_validos();
+		for(int i = 0; i < 4; i++) {
+			if(adjacentes[i] != null) {
+				Componente brisa = new Brisa(adjacentes[i].getX(), adjacentes[i].getY());
+				caverna.adicionarComponente(adjacentes[i], brisa);
+			}
+		}
 	}
 
 	public String toString() {
 		return "B";
 	}
+
 	
 }
