b265228e4c42d732829a52368eb9528d63e47a98
HenriquePManoel <102121798+HenriquePManoel@users.noreply.github.com>
2022-05-15 22:55:46 -0300

    Update Wumpus.java

--- lab05/src/pt/c04task/l05wumpus/Wumpus.java
+++ lab05/src/pt/c04task/l05wumpus/Wumpus.java
@@ -2,12 +2,12 @@ package pt.c40task.l05wumpus;
 
 public class Wumpus extends Componente{
 
-	public Wumpus(int x, int y, Caverna caverna) {
-		super(x, y, caverna);
+	public Wumpus(int x, int y) {
+		super(x, y);
 		Posicao adjacentes[] = posicao.adjacentes_validos();
 		for(int i = 0; i < 4; i++) {
 			if(adjacentes[i] != null) {
-				Componente fedor = new Fedor(adjacentes[i].getX(), adjacentes[i].getY(), caverna);
+				Componente fedor = new Fedor(adjacentes[i].getX(), adjacentes[i].getY());
 				caverna.adicionarComponente(adjacentes[i], fedor);
 			}
 		}
