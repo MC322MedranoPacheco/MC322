978cfb6a8b5abfbe8207ca1a8a5acc04b97c6d71
HenriquePManoel <102121798+HenriquePManoel@users.noreply.github.com>
2022-05-18 20:24:48 -0300

    comentário no construtor

--- lab05/src/pt/c40task/l05wumpus/Wumpus.java
+++ lab05/src/pt/c40task/l05wumpus/Wumpus.java
@@ -2,6 +2,7 @@ package pt.c40task.l05wumpus;
 
 public class Wumpus extends Componente{
 
+	/* construtor que coloca os componentes secundarios do mesmo nas salas corretas */
 	public Wumpus(int x, int y) {
 		super(x, y);
 		Posicao adjacentes[] = posicao.adjacentes_validos();
