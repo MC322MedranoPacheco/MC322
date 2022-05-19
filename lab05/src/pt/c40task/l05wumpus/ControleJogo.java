3c4ffeec9987e928beb40f9c01cdb57c0605ee6e
HenriquePManoel <102121798+HenriquePManoel@users.noreply.github.com>
2022-05-18 22:16:13 -0300

    erro de digitação implementado

--- lab05/src/pt/c40task/l05wumpus/ControleJogo.java
+++ lab05/src/pt/c40task/l05wumpus/ControleJogo.java
@@ -25,52 +25,59 @@ public class ControleJogo {
 	/* Funcao que recebe um comando para o heroi e o realiza, atualizando o valor da pontuacao conforme o esperado */
 	public static void movimento(String comando) {
 		Posicao posDest = new Posicao(heroi.getPosicao());
-		switch (comando) {
-		case "w":
-			posDest.setY(posDest.getY() - 1);
-			pontuacao += heroi.moverHeroi(posDest); // Adiciona a pontuacao o tanto de pontos feito pelo heroi e realiza o movimento
-			if(!heroi.getVivo()) estadoJogo = 'L'; // Caso o heroi tenha morrido, muda-se o estado do jogo
-			else if(heroi.ganhou()) { 
-				estadoJogo = 'W';
-				pontuacao += 1000;
+		try{
+			switch (comando) {
+			case "w":
+				posDest.setY(posDest.getY() - 1);
+				pontuacao += heroi.moverHeroi(posDest); // Adiciona a pontuacao o tanto de pontos feito pelo heroi e realiza o movimento
+				if(!heroi.getVivo()) estadoJogo = 'L'; // Caso o heroi tenha morrido, muda-se o estado do jogo
+				else if(heroi.ganhou()) { 
+					estadoJogo = 'W';
+					pontuacao += 1000;
+				}
+				break;
+			case "d":	
+				posDest.setX(posDest.getX() + 1);
+				pontuacao += heroi.moverHeroi(posDest);
+				if(!heroi.getVivo()) estadoJogo = 'L';
+				else if(heroi.ganhou()) { 
+					estadoJogo = 'W';
+					pontuacao += 1000;
+				}
+				break;
+			case "a":	
+				posDest.setX(posDest.getX() - 1);
+				pontuacao += heroi.moverHeroi(posDest);
+				if(!heroi.getVivo()) estadoJogo = 'L';
+				else if(heroi.ganhou()) { 
+					estadoJogo = 'W';
+					pontuacao += 1000;
+				}
+				break;
+			case "s":	
+				posDest.setY(posDest.getY() + 1);
+				pontuacao += heroi.moverHeroi(posDest);
+				if(!heroi.getVivo()) estadoJogo = 'L';
+				else if(heroi.ganhou()) { 
+					estadoJogo = 'W';
+					pontuacao += 1000;
+				}
+				break;
+			case "k":
+				heroi.setEquipado(true); // Equipa a flecha
+				break; 
+			case "c":
+				heroi.pegarOuro(); // Pega o ouro
+				break;
+			case "q":
+				estadoJogo = 'q';
+				break;
+			default:
+				throw new IllegalArgumentException("Digito invalido, tente novamente");
 			}
-			break;
-		case "d":	
-			posDest.setX(posDest.getX() + 1);
-			pontuacao += heroi.moverHeroi(posDest);
-			if(!heroi.getVivo()) estadoJogo = 'L';
-			else if(heroi.ganhou()) { 
-				estadoJogo = 'W';
-				pontuacao += 1000;
-			}
-			break;
-		case "a":	
-			posDest.setX(posDest.getX() - 1);
-			pontuacao += heroi.moverHeroi(posDest);
-			if(!heroi.getVivo()) estadoJogo = 'L';
-			else if(heroi.ganhou()) { 
-				estadoJogo = 'W';
-				pontuacao += 1000;
-			}
-			break;
-		case "s":	
-			posDest.setY(posDest.getY() + 1);
-			pontuacao += heroi.moverHeroi(posDest);
-			if(!heroi.getVivo()) estadoJogo = 'L';
-			else if(heroi.ganhou()) { 
-				estadoJogo = 'W';
-				pontuacao += 1000;
-			}
-			break;
-		case "k":
-			heroi.setEquipado(true); // Equipa a flecha
-			break; 
-		case "c":
-			heroi.pegarOuro(); // Pega o ouro
-			break;
-		case "q":
-			estadoJogo = 'q';
-			break;
+		}
+		catch(IllegalArgumentException e) {
+			System.out.println("Erro: " + e.getMessage())
 		}
 	}
 	
